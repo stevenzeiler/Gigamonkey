1456c4b08b893fed65311cbee48e64686dc80b29
Steven Zeiler <me@stevezeiler.com>
2021-08-02 00:25:26 +0200

    Install some dependencies with conan
    Remove local path to bitcoin-sv from compile.sh
    Indicate packages in conanfile that must conanified

---
+++ compile.sh
@@ -0,0 +1,4 @@
+cd cmake
+conan install ..
+cd ..
+cmake -S . -B build/
--- conanfile.py
+++ conanfile.py
@@ -4,10 +4,15 @@ class ConanPackage(ConanFile):
     name = 'gigamonkey'
     version = "0.1.0"
 
-    generators = 'cmake_find_package'
+    generators = ['cmake', 'cmake_find_package']
 
     requires = [
+        ('boost/1.76.0'),
         ('cryptopp/8.5.0'),
+        ('nlohmann_json/3.2.0'),
+        ('gmp/6.2.1'),
+        #(github.com/bitcoin-sv/bitcoin-sv)
+        #(github.com/bitcoin-core/secp256k1)
     ]
 
     default_options = (
